437ad78ff24c3c2015e641d124a238461bed6627
tinapayy <99814732+tinapayy@users.noreply.github.com>
2022-04-29 22:52:03 +0800

    Add files via upload

---
+++ Lecture7/Assignments/as3.c
@@ -0,0 +1,35 @@
+/*** SAMPLE CODE 3 || PAGE 36 ***/
+
+/* Computing for the average rating done by each reviewer ft. Jagged Array */
+
+#include <stdio.h>
+
+#define N_REVIEWERS 3
+#define N_MOVIES 4
+
+int main(){
+
+    double sum_per_reviewer, ave_per_reviewer;
+
+    double ratings[N_REVIEWERS][N_MOVIES] = {
+        {4, 6, 2, 5},
+        {7, 9, 4},
+        {6, 9, 3, 7}
+    };
+
+    for (int reviewer = 0; reviewer < N_REVIEWERS; reviewer++){
+
+        printf("Reviewer%d -->  \t", reviewer+1);
+        sum_per_reviewer = 0;
+
+        for (int movie = 0; movie < N_MOVIES; movie++){
+            printf("%.0f ", ratings[reviewer][movie]);
+            sum_per_reviewer += ratings[reviewer][movie];
+        }
+
+        ave_per_reviewer = sum_per_reviewer / N_MOVIES;
+        printf("\t --> Average %.1f", ave_per_reviewer);
+        printf("\n");
+    }
+    return 0;
+}
